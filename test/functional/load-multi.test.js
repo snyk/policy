1374a055dabcf3e485c020f3acf9a0a108c994aa
Remy Sharp <remy@remysharp.com>
2016-03-09 17:42:41 +0000

    fix: swallow fs.stat always
    Since the loose command will pick up files that missing

---
+++ test/functional/load-multi.test.js
@@ -0,0 +1,12 @@
+var test = require('tap-only');
+var policy = require('../../');
+var fixtures = __dirname + '/../fixtures';
+var dir1 = fixtures + '/empty';
+var dir2 = fixtures + '/patch';
+
+test('multiple directories, one with policy, one without', function (t) {
+  return policy.load([dir1, dir2], { loose: true }).then(function (res) {
+    t.ok(res.patch, 'patch property is present');
+    t.equal(Object.keys(res.patch).length, 2, 'patches found');
+  });
+});
