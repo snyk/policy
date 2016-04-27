4e7c74b4a95c6402901d65a3d1a9439799c9f901
Remy Sharp <remy@remysharp.com>
2016-04-27 18:34:31 +0100

    test: using the `.add` function

---
+++ test/functional/append.test.js
@@ -0,0 +1,20 @@
+var test = require('tap-only');
+var Promise = require('es6-promise').Promise; // jshint ignore:line
+var policy = require('../../');
+var fixtures = __dirname + '/../fixtures/issues/SC-1106/';
+var withDash = fixtures + '/pre-update.snyk';
+var needsFixing = require('../../lib/parser/v1').needsFixing;
+
+test('merging new policy data does not corrupt', function (t) {
+  return policy.load(withDash).then(function (policy) {
+    policy.addIgnore({
+      id: 'npm:hawk:20160119',
+      path: 'octonode > request > hawk',
+      expires: new Date('2016-05-24T13:46:19.066Z'),
+      reason: 'none given',
+    });
+
+    t.equal(needsFixing(policy.ignore), false, 'no corruption');
+    t.equal(Object.keys(policy.ignore['npm:hawk:20160119']).length, 3, 'has 3 rules');
+  });
+});
