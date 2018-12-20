e31c1a4f165a0593b81f259b3da26b5c8299bfbe
Kirill Efimov <kirill@snyk.io>
2018-12-20 10:24:23 +0200

    fix: remove broken patch from parsing result

---
+++ test/functional/BST-264.test.js
@@ -0,0 +1,10 @@
+var test = require('tap-only');
+var policy = require('../../');
+
+test('broken patch should not be in output', function (t) {
+  return policy
+    .load(__dirname + '/../fixtures/issues/BST-264/missing-path-to-package.snyk', {loose: true})
+    .then(function (policy) {
+      t.deepEqual(policy.patch, {}, 'patch section is empty');
+    });
+});
