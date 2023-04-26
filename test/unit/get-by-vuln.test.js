7d5cac65a480e729212f75a995e46d86a96c6817
gclapperton <gclapperton@users.noreply.github.com>
2023-04-26 10:10:50 -0400

    fix: migrate from promise-fs to fs/promises

--- test/unit/get-by-vuln.test.js
+++ test/unit/get-by-vuln.test.js
@@ -1,6 +1,6 @@
 const test = require('tap-only');
 const fixtures = __dirname + '/../fixtures';
-const fs = require('promise-fs');
+const { promises: fs } = require('fs');
 const getByVuln = require('../../lib/match').getByVuln;
 const loadFromText = require('../../').loadFromText;
 const policy = require(fixtures + '/ignore/parsed.json');
