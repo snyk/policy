35187704fb21bf3c7b4929de39e0c3d3b06a5fbd
Gareth <gclapperton@users.noreply.github.com>
2023-06-30 13:40:06 -0400

    fix: check for old snyk file format (#124)

--- test/unit/old-format.test.ts
+++ test/unit/old-format.test.ts
@@ -1,12 +1,9 @@
 import { expect, test } from 'vitest';
 import * as policy from '../../lib';
 
-test('test sensibly bails if gets an old .snyk format', () => {
-  expect(() =>
-    policy.load(__dirname + '/../fixtures/old-snyk-config/').catch((e) => {
-      expect(e.message).toBe('old, unsupported .snyk format detected');
-      expect(e.code).toBe('OLD_DOTFILE_FORMAT');
-      throw e;
-    })
-  ).rejects.toThrow();
+test('test sensibly bails if gets an old .snyk format', async () => {
+  const oldSnykFormatError: NodeJS.ErrnoException = new Error('old, unsupported .snyk format detected');
+  oldSnykFormatError.code = 'OLD_DOTFILE_FORMAT';
+
+  await expect(() => policy.load(__dirname + '/../fixtures/old-snyk-config/')).rejects.toThrow(oldSnykFormatError);
 });
