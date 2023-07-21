4438b4fbc708742a3c4ab897f53167c52b8ddeac
Gareth <gclapperton@users.noreply.github.com>
2023-07-21 09:20:04 -0400

    feat: bundle library and emit types (#125)
    BREAKING CHANGE: access to nested packages is no longer available

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      fileName: 'index', // extension added automatically based on `format`
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+        // externalize node dependencies
+        external: ['fs', 'path', 'url', 'util'],
+      },
+  },
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
+  test: {
+    globals: true,
+    dir: 'test',
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'html'],
+
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
+})
